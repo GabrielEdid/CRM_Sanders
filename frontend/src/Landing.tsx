f823b0f4cb34d03e4623ee3fba77753cff109c98
GabrielEdid <gabrieledid28@gmail.com>
2024-10-06 14:26:50 -0600

    Cambios después del merge conflictivo, se arreglaron imagenes y colores

--- frontend/src/Landing.tsx
+++ frontend/src/Landing.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useRef } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import styles from "./Landing.module.css";
-import Logo from "./img/Logo.jpg";
+import Logo from "./img/Logo.png";
+import Logo_Sanders from "../assets/images/Logo_Sanders.jpeg";
+import Equipo from "./img/Equipo.jpeg";
 import Agua from "./img/Agua.jpg";
 import Donaciones from "./img/Donaciones.jpg";
 
@@ -47,10 +49,14 @@ const Landing = () => {
         return;
       }
 
-      const stripe = await loadStripe("pk_test_51Q5ad0GyaQDHPtc7o5doiVK2aeP5CkolWLeYKHWvpic4At3c3yHG3cIGPC5ejMD3xaCpyVaW8f83nAMAiOIODEHd00EFSvMKYd");
+      const stripe = await loadStripe(
+        "pk_test_51Q5ad0GyaQDHPtc7o5doiVK2aeP5CkolWLeYKHWvpic4At3c3yHG3cIGPC5ejMD3xaCpyVaW8f83nAMAiOIODEHd00EFSvMKYd"
+      );
 
       if (!stripe) {
-        setErrorMessage("Failed to initialize Stripe. Please try again.");
+        setErrorMessage(
+          "Error al inicializar Stripe. Por favor, inténtalo de nuevo."
+        );
         setIsLoading(false);
         return;
       }
@@ -58,10 +64,14 @@ const Landing = () => {
       const result = await stripe.redirectToCheckout({ sessionId });
 
       if (result.error) {
-        setErrorMessage(result.error.message);
+        setErrorMessage(
+          result.error.message ? result.error.message : "Ocurrió un error."
+        );
       }
     } catch (error) {
-      setErrorMessage("An unexpected error occurred. Please try again.");
+      setErrorMessage(
+        "Ocurrión un error inesperado. Por favor, inténtalo de nuevo."
+      );
     } finally {
       setIsLoading(false);
     }
@@ -77,29 +87,111 @@ const Landing = () => {
         <div className={styles.logo}>Fundación Sanders</div>
         <nav>
           <ul className={styles.navLinks}>
-            <li><a href="#about">Nosotros</a></li>
-            <li><a href="#activities">Actividades</a></li>
-            <li><a href="#donations">Donaciones</a></li>
-            <li><a href="#contact">Contacto</a></li>
+            <li>
+              <a href="#about">Nosotros</a>
+            </li>
+            <li>
+              <a href="#activities">Actividades</a>
+            </li>
+            <li>
+              <a href="#donations">Donaciones</a>
+            </li>
+            <li>
+              <a href="#contact">Contacto</a>
+            </li>
           </ul>
         </nav>
       </header>
 
       <section className={styles.heroSection}>
-        <h1 className={styles.heroTitle}>¡Bienvenido a la Fundación Sanders!</h1>
+        <h1 className={styles.heroTitle}>
+          ¡Bienvenido a la Fundación Sanders!
+        </h1>
+        <img
+          src={Logo_Sanders}
+          alt="Nosotros"
+          className={styles.sectionImage}
+        />
         <p className={styles.heroSubtitle}>
-          Nuestra misión es ayudar a los menos favorecidos a través de la tecnología y proveerles agua limpia.
+          Nuestra misión es ayudar a los menos favorecidos a través de la
+          tecnología y proveerles agua limpia.
         </p>
         <button className={styles.heroButton} onClick={scrollToDonationSection}>
           Haz una donación
         </button>
       </section>
 
-      <section id="donations" className={`${styles.infoSection}`} ref={donationSectionRef}>
+      <section id="about" className={`${styles.infoSection} ${styles.reverse}`}>
+        <img src={Logo} alt="Nosotros" className={styles.sectionImage} />
+        <div className={styles.textContent}>
+          <h2>Nosotros</h2>
+          <p>
+            Fundación Sanders A.C., es una Asociación Civil sin fines de lucro
+            creada en el año 2016 por iniciativa del empresario mexicano
+            Guillermo Sanders Acedo (1935-2019), para contribuir a la mejora de
+            la calidad de vida en grupos sociales en situación de
+            vulnerabilidad, mediante la promoción de la salud sexual y
+            reproductiva, la nutrición comunitaria y el abastecimiento de agua.
+          </p>
+        </div>
+      </section>
+
+      <section id="activities" className={styles.infoSection}>
+        <div className={styles.textContent}>
+          <h2>Actividades</h2>
+          <h1>TEC HIDALGO</h1>
+          <p>
+            Tuvimos la oportunidad de platicar en HIDALMUN 2022 compartiendo con
+            alumnos, directivos y líderes sobre el surgimiento de Fundación
+            Sanders A. C.
+          </p>
+          <h1>HIPGIVE</h1>
+          <p>
+            Es la plataforma de herramientas digitales de “Hispanics in
+            Philanthropy” que posibilita la movilización de recursos para
+            proyectos de impacto social en el continente americano.
+          </p>
+          <h1>INSTITUTO TLALPAN</h1>
+          <p>
+            Con el instituto Tlalpan llevamos a cabo programa con alumnos de 3°
+            de preparatoria.
+          </p>
+        </div>
+
+        <div className={styles.videoSection}>
+          <a
+            href="https://www.youtube.com/watch?v=G0igvtwkJv4&t=3s"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.videoLink}
+          >
+            Ver video en YouTube
+          </a>
+          <div className={styles.videoWrapper}>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/G0igvtwkJv4"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </section>
+
+      <section
+        id="donations"
+        className={`${styles.infoSection}`}
+        ref={donationSectionRef}
+      >
         <img src={Agua} alt="Donaciones" className={styles.sectionImage} />
         <div className={styles.textContent}>
           <h2>Donaciones</h2>
-          <p>Con tu apoyo, podemos seguir llevando agua y educación a más niños. ¡Haz tu donación hoy!</p>
+          <p>
+            Con tu apoyo, podemos seguir llevando agua y educación a más niños.
+            ¡Haz tu donación hoy!
+          </p>
           <form
             onSubmit={(e) => {
               e.preventDefault();
@@ -146,13 +238,19 @@ const Landing = () => {
         <div className={styles.textContent}>
           <h2>Contacto</h2>
           <h1>DIRECCIÓN</h1>
-          <p>Melchor Ocampo 193, Torre A, Piso 1, CP 11300 Col. Verónica Anzures</p>
+          <p>
+            Melchor Ocampo 193, Torre A, Piso 1, CP 11300 Col. Verónica Anzures
+          </p>
           <h1>TELÉFONO</h1>
           <p>55 1707-6203</p>
           <h1>CORREO</h1>
           <p>contacto@sanders.com.mx</p>
         </div>
-        <img src={Donaciones} alt="Donaciones" className={styles.sectionImage1} />
+        <img
+          src={Donaciones}
+          alt="Donaciones"
+          className={styles.sectionImage1}
+        />
       </section>
 
       <footer className={styles.footer}>
